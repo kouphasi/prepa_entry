491f1597dd043ba1132902c7d4e82da23c1273bf
phasi <koumiyom4@gmail,com>
2022-02-13 00:30:42 +0900

    made the base

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4qzryWZuwgjo8oazGRk2OaG26FyFsIto",
+  authDomain: "prepa-entry.firebaseapp.com",
+  projectId: "prepa-entry",
+  storageBucket: "prepa-entry.appspot.com",
+  messagingSenderId: "219127957075",
+  appId: "1:219127957075:web:ce4e4ca568d1f02e87e8a6",
+  measurementId: "G-WWJT0ZFP1H",
+};
+
+firebase.initializeApp(firebaseConfig);
